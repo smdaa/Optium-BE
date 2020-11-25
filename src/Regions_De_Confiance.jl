e305f78dc7c497223bc5221654173b75a3b98c9e
smdaa <saadmdaa03@gmail.com>
2020-11-25 17:10:12 +0100

    region_confiance +

--- src/Regions_De_Confiance.jl
+++ src/Regions_De_Confiance.jl
@@ -74,11 +74,51 @@ function Regions_De_Confiance(algo,f::Function,gradf::Function,hessf::Function,x
         Tol_rel = options[9]
     end
 
-    n = length(x0)
-    xmin = zeros(n)
-    fxmin = f(xmin)
-    flag = 0
+    convergence = false
+    stagnation_x = false
+    stagnation_f = false
+
     nb_iters = 0
 
+    if algo == "cauchy"
+        while ((! convergence) || (! stagnation_x) || (! stagnation_f)) && (nb_iters <= max_iter) 
+            # a. calculer approximativement s_k solution du sous-problème (2.1)
+            s, e = Pas_De_Cauchy(gradf(x0), hessf(x0), delta0)
+
+            # b. Evaluer f(x_k + s_k) et rho_k
+            rho = (f(x0) - f(x0 + s)) / (- transpose(gradf(x0)) * s + (1/2) * transpose(s) * hessf(x0) * s)
+
+            # c. Mettre à jour l’itéré courant
+            if rho >= eta1
+                x_temp = x0
+                x0 = x0 + s
+                convergence = (norm(gradf(x0)) <= max(Tol_rel * norm(gradf(x0)), Tol_abs))
+                stagnation_x = norm(x_temp - x0) <= max(Tol_rel * norm(x_temp), Tol_abs)
+                stagnation_f = norm(f(x0) - f(x_temp)) <= max(Tol_rel * norm(f(x_temp)), Tol_abs)
+            end
+
+            # d. Mettre à jour la région de confiance
+            if rho > eta2
+                delta0 = min(gamma2 * delta0, deltaMax)
+            elseif rho < eta1
+                delta0 = delta0 * gamma1
+            end
+
+            nb_iters = nb_iters + 1
+        end
+    end
+
+    n = length(x0)
+    xmin = x0
+    fxmin = f(xmin)
+    if convergence 
+        flag = 0
+    elseif stagnation_x
+        flag = 1
+    elseif stagnation_f
+        flag = 2
+    elseif (nb_iters > max_iter)
+        flag = 3
+    end
     return xmin, fxmin, flag, nb_iters
 end
