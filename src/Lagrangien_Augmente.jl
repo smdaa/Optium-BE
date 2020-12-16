50db76dcdae5ce0fdad86c7209863c192740aefa
smdaa <saadmdaa03@gmail.com>
2020-12-16 17:35:42 +0100

    ko

--- src/Lagrangien_Augmente.jl
+++ src/Lagrangien_Augmente.jl
@@ -95,17 +95,20 @@ function Lagrangien_Augmente(algo,fonc::Function,contrainte::Function,gradfonc::
   while (iter <= itermax)
     LA(x) = fonc(x) + transpose(lambda) * contrainte(x) + (1 / 2) * mu * (norm(contrainte(x)) ^ 2)
     grad_LA(x) = gradfonc(x) + transpose(lambda) * grad_contrainte(x) + mu * grad_contrainte(x) * contrainte(x)
-    hess_LA(x) = hessfonc(x) + transpose(lambda) * hess_contrainte(x) + mu * (grad_contrainte(x) * transpose(grad_contrainte(x)) )
+    hess_LA(x) = hessfonc(x) + transpose(lambda) * hess_contrainte(x) + mu * (hess_contrainte(x) * contrainte(x) + (grad_contrainte(x) * transpose(grad_contrainte(x)) ))
 
     #a
     if algo == "newton"
-      xl, ~ = Algorithme_De_Newton(LA, grad_LA, hess_LA, x0, [])
+      options_newton = []
+      xl, ~ = Algorithme_De_Newton(LA, grad_LA, hess_LA, x0, options_newton)
     elseif algo == "cauchy"
-      xl, ~ = Regions_De_Confiance("cauchy", LA, grad_LA, hess_LA, x0, [])
+      options_cauchy = []
+      xl, ~ = Regions_De_Confiance("cauchy", LA, grad_LA, hess_LA, x0, options_cauchy)
     elseif algo == "gct"
-      xl, ~ = Regions_De_Confiance("gct", LA, grad_LA, hess_LA, x0, [])
+      options_gct = []
+      xl, ~ = Regions_De_Confiance("gct", LA, grad_LA, hess_LA, x0, options_gct)
     end
-    convergence = (norm(grad_LA(xl)) <= tol * norm(grad_LA0)) && (norm(contrainte(xl)) <= tol * norm(contrainte(x0)))
+    convergence = (norm(grad_LA(xl)) <= max(tol * norm(grad_LA0), epsilon)) && (norm(contrainte(xl)) <= max(tol * norm(contrainte(x0)), epsilon))
     if convergence 
       xmin = xl
       break
